f8dff68bc010a5fc90f4011c8078a3b4a5926a3e
Meghan Stang <mstang15@gmail.com>
2018-09-12 14:13:44 -0600

    adds iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,29 @@ class Market
       vendor.inventory.include?(item)
     end
   end
+
+  def sorted_item_list
+    items_array = @vendors.map do |vendor|
+                    vendor.inventory.keys
+                  end
+    items_array.flatten.uniq.sort
+  end
+
+  def find_quantity(item)
+    @vendors.inject(0) do |sum, vendor|
+      if vendor.inventory.include?(item)
+        vendor.inventory[item] + sum
+      else
+        0 + sum
+      end
+    end
+  end
+
+  def total_inventory
+    total_inventory = Hash.new{}
+    sorted_item_list.map do |item|
+      total_inventory[item] = find_quantity(item)
+    end
+    return total_inventory
+  end
 end
