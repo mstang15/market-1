52e04a9bc39799537a9a83c2ccfdbee4d2f34ca3
Meghan Stang <mstang15@gmail.com>
2018-09-12 13:28:22 -0600

    finishes iteration 1

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,6 +1,26 @@
 class Vendor
-  attr_reader :name 
+  attr_reader :name, :inventory
   def initialize(name)
     @name = name
+    @inventory = Hash.new{}
+  end
+
+  def check_stock(item)
+    found_item = @inventory.keys.find do |food|
+                  food == item
+                end
+    if found_item == nil
+      0
+    else
+      return @inventory[found_item]
+    end
+  end
+
+  def stock(item, quantity)
+    if check_stock(item) == 0
+      @inventory[item] = quantity
+    else
+      @inventory[item] += quantity
+    end
   end
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require 'pry'
 
 
 class VendorTest <Minitest::Test
@@ -15,4 +16,45 @@ class VendorTest <Minitest::Test
 
     assert_equal "Rocky Mountain Fresh", vendor.name
   end
+
+  def test_it_returns_empty_hash_for_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    empty_hash = {}
+
+    assert_equal empty_hash, vendor.inventory
+  end
+
+  def test_it_can_check_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal 0, vendor.check_stock("Peaches")
+  end
+
+  def test_it_can_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    vendor.stock("Peaches", 30)
+
+    assert_equal 30, vendor.check_stock("Peaches")
+  end
+
+  def test_it_can_add_to_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock("Peaches", 25)
+    vendor.stock("Peaches", 30)
+    assert_equal 55, vendor.check_stock("Peaches")
+  end
+
+  def test_it_can_add_more_than_one_item_to_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock("Peaches", 25)
+    vendor.stock("Peaches", 30)
+    vendor.stock("Tomatoes", 12)
+
+    expected = {"Peaches" => 55, "Tomatoes" => 12}
+
+    assert_instance_of Hash, vendor.inventory
+    assert_equal  expected, vendor.inventory
+  end
+
 end
